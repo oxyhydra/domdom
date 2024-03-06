7d7efb3e3671062a8896f5ccc897f6450fa7756d
Hydraz <94378325+oxyhydra@users.noreply.github.com>
2024-03-06 22:52:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML =  "Yayyyy daddy love youuu more oun sml";
+  question.innerHTML =  "Yayyyy daddy love youuu more oun sml 💋";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
